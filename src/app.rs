0851c95eadb469c5d0bad5591bbcb68e9e0330c7
Zachary Brown <zach@blackmaple.net>
2026-05-17 20:04:12 -0600

    Initial font, colors, text redux.

--- src/app.rs
+++ src/app.rs
@@ -4,15 +4,11 @@ use yew::prelude::*;
 pub fn App() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.svg" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-            <div class="card">
-                <div class="card-title">{"Some Card"}</div>
-                <div class="card-body">
-                    {"Some Body"}
-                </div>
-            </div>
+            <h1>{ "black maple software" }</h1>
+
+            <footer>
+                <span class="info">{ "made with Rust + Yew + " }<i class="heart" /></span>
+            </footer>
         </main>
     }
 }
